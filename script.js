55d009f1e6852b258b83aec0a254574f3aa0dc19
Goiat <tiagosilva2517.ts@gmail.com>
2025-07-13 21:59:04 -0300

    js-java

--- script.js
+++ script.js
@@ -1,47 +1,57 @@
-const localStorageKey = 'to-do-list-tgs';
+function newTask() {
+    const input = document.getElementById("taskInput");
+    input.style.border = '';
 
-function validateNewTask(){
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");    
-    let inputValue = document.getElementById("taskInput").value
-    let existe = values.find(x => x.name == inputValue)
-    return !existe ? false : true
-}
-function newTask(){ 
-    let input = document.getElementById("taskInput");
-    input.style.border = ''
-    //validação do input
-    if(!input.value){
-        alert("Digite uma tarefa!")
-        input.style.border = '1px solid red'
+    if (!input.value) {
+        alert("Digite uma tarefa!");
+        input.style.border = '1px solid red';
+        return;
     }
-    else if(validateNewTask()){
-        alert('Já existe esta tarefa')
-    }
-    else{
-        let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-        values.push({
-            name: input.value
-        })
-        localStorage.setItem(localStorageKey, JSON.stringify(values));
-        showValues()
-    }
-    input.value = ''
+
+    fetch("http://localhost:8080/tasks", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name: input.value })
+    })
+    .then(response => {
+        if (!response.ok) throw new Error("Erro ao salvar tarefa");
+        input.value = "";
+        showValues();
+    })
+    .catch(error => {
+        console.error("Erro:", error);
+        alert("Erro ao adicionar tarefa.");
+    });
 }
-function showValues(){
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    let list = document.getElementById("to-do-list");
-    list.innerHTML = ""; // Limpa a lista antes de mostrar os valores   
-    for(let i = 0; i < values.length; i++){
-        list.innerHTML += `<li>${values[i]['name']}<button id ='buttonok' onclick ='removeItem("${values[i]['name']}")'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
-  <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
-</svg></button></li>`;
-    }
+
+function showValues() {
+    fetch("http://localhost:8080/tasks")
+        .then(response => response.json())
+        .then(data => {
+            const list = document.getElementById("to-do-list");
+            list.innerHTML = "";
+
+            data.forEach(task => {
+                list.innerHTML += `
+                    <li>${task.name}
+                        <button id='buttonok' onclick='removeItem(${task.id})'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
+                            </svg>
+                        </button>
+                    </li>
+                `;
+            });
+        });
 }
-function removeItem(data){
-    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
-    let index = values.findIndex(x => x.name == data)
-    values.splice(index,1)
-    localStorage.setItem(localStorageKey, JSON.stringify(values));
-    showValues()
+
+function removeItem(id) {
+    fetch(`http://localhost:8080/tasks/${id}`, {
+        method: "DELETE"
+    })
+    .then(() => showValues());
 }
-showValues()
+
+showValues();
